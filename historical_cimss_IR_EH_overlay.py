f17f205e125d143d6417087b449454be377895f1
hsushipei1 <hsushipei1@gmail.com>
2017-05-30 15:08:58 +0800

    Add files via upload
    Solve issues relating to calling system command under Windows

--- historical_cimss_IR_EH_overlay.py
+++ historical_cimss_IR_EH_overlay.py
@@ -18,21 +18,21 @@ while startd <= endd:
 		# Cropping
 		os.system("convert ./Indian/"+strd_str+"/IRImageNHCEnhancement/"+strd_str+"."+hh+".Indian.IRImageNHCEnhancement.png \
 					-crop 1080x666+0+0 \
-					./processing_his_cimss/cropped_io_"+strd_str+"."+hh+".GIF  ;\
-				convert ./Australia/"+strd_str+"/IRImageNHCEnhancementWest/"+strd_str+"."+hh+".Australia.IRImageNHCEnhancementWest.png \
+					./processing_his_cimss/cropped_io_"+strd_str+"."+hh+".GIF")
+		os.system("convert ./Australia/"+strd_str+"/IRImageNHCEnhancementWest/"+strd_str+"."+hh+".Australia.IRImageNHCEnhancementWest.png \
 					-crop 868x680+260+0 \
-					./processing_his_cimss/cropped_wau_"+strd_str+"."+hh+".GIF ;\
-				convert ./NWPacific/"+strd_str+"/IRImageNHCEnhancement/"+strd_str+"."+hh+".NWPacific.IRImageNHCEnhancement.png \
+					./processing_his_cimss/cropped_wau_"+strd_str+"."+hh+".GIF")
+		os.system("convert ./NWPacific/"+strd_str+"/IRImageNHCEnhancement/"+strd_str+"."+hh+".NWPacific.IRImageNHCEnhancement.png \
 					-crop 1080x644+0+0 \
 					./processing_his_cimss/cropped_wnp_"+strd_str+"."+hh+".GIF")
 
 		# Overlaying
-		os.system("convert -size 1965x1242 xc:black ./processing_his_cimss/bk_"+strd_str+"."+hh+".png ;\
-				convert -composite -geometry +0+277.1 ./processing_his_cimss/bk_"+strd_str+"."+hh+".png \
-					./processing_his_cimss/cropped_io_"+strd_str+"."+hh+".GIF ./processing_his_cimss/out1_"+strd_str+"."+hh+".png ;\
-				convert -composite -geometry +872+555.1 ./processing_his_cimss/out1_"+strd_str+"."+hh+".png \
-					./processing_his_cimss/cropped_wau_"+strd_str+"."+hh+".GIF ./processing_his_cimss/out2_"+strd_str+"."+hh+".png ;\
-				convert -composite -geometry +889+0 ./processing_his_cimss/out2_"+strd_str+"."+hh+".png \
+		os.system("convert -size 1965x1242 xc:black ./processing_his_cimss/bk_"+strd_str+"."+hh+".png")
+		os.system("convert -composite -geometry +0+277.1 ./processing_his_cimss/bk_"+strd_str+"."+hh+".png \
+					./processing_his_cimss/cropped_io_"+strd_str+"."+hh+".GIF ./processing_his_cimss/out1_"+strd_str+"."+hh+".png")
+		os.system("convert -composite -geometry +872+555.1 ./processing_his_cimss/out1_"+strd_str+"."+hh+".png \
+					./processing_his_cimss/cropped_wau_"+strd_str+"."+hh+".GIF ./processing_his_cimss/out2_"+strd_str+"."+hh+".png")
+		os.system("convert -composite -geometry +889+0 ./processing_his_cimss/out2_"+strd_str+"."+hh+".png \
 					./processing_his_cimss/cropped_wnp_"+strd_str+"."+hh+".GIF ./overlaid/overlaid_cimss_IR_"+strd_str+"."+hh+".png")
 
 	startd = startd + deltad
--- realtime_cimss_IR_EH_overlay.py
+++ realtime_cimss_IR_EH_overlay.py
@@ -12,15 +12,15 @@ urllib.urlretrieve("http://tropic.ssec.wisc.edu/real-time/westpac/images/irngms.
 urllib.urlretrieve("http://tropic.ssec.wisc.edu/real-time/austwest/images/irngms.GIF", "./processing_rt_cimss/wau.GIF")
 
 # Crop images
-os.system("convert ./processing_rt_cimss/io.GIF -crop 1080x666+0+0 ./processing_rt_cimss/io2.GIF ;\
-		convert ./processing_rt_cimss/wau.GIF -crop 868x680+260+0 ./processing_rt_cimss/wau2.GIF ;\
-		convert ./processing_rt_cimss/wnp.GIF -crop 1080x644+0+0 ./processing_rt_cimss/wnp2.GIF")
+os.system("convert ./processing_rt_cimss/io.GIF -crop 1080x666+0+0 ./processing_rt_cimss/io2.GIF")
+os.system("convert ./processing_rt_cimss/wau.GIF -crop 868x680+260+0 ./processing_rt_cimss/wau2.GIF")
+os.system("convert ./processing_rt_cimss/wnp.GIF -crop 1080x644+0+0 ./processing_rt_cimss/wnp2.GIF")
 
 # Overlay images
-os.system("convert -size 1965x1242 xc:black ./processing_rt_cimss/bk.png ;\
-		convert -composite -geometry +0+277.1 ./processing_rt_cimss/bk.png ./processing_rt_cimss/io2.GIF ./processing_rt_cimss/out.png ;\
-		convert -composite -geometry +872+555.1 ./processing_rt_cimss/out.png ./processing_rt_cimss/wau2.GIF ./processing_rt_cimss/out2.png ;\
-		convert -composite -geometry +889+0 ./processing_rt_cimss/out2.png ./processing_rt_cimss/wnp2.GIF ./overlaid_cimss_IR_latest.png")
+os.system("convert -size 1965x1242 xc:black ./processing_rt_cimss/bk.png")
+os.system("convert -composite -geometry +0+277.1 ./processing_rt_cimss/bk.png ./processing_rt_cimss/io2.GIF ./processing_rt_cimss/out.png")
+os.system("convert -composite -geometry +872+555.1 ./processing_rt_cimss/out.png ./processing_rt_cimss/wau2.GIF ./processing_rt_cimss/out2.png")
+os.system("convert -composite -geometry +889+0 ./processing_rt_cimss/out2.png ./processing_rt_cimss/wnp2.GIF ./overlaid_cimss_IR_latest.png")
 
 # Delete intermediate files
 shutil.rmtree("./processing_rt_cimss")
